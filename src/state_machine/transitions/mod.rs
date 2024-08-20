ea892bf4761c17a284f1f868687ef89f6c16f281
Michael Gonzalez <michaelgonzalez.code@gmail.com>
2024-08-19 20:02:52 -0400

    decouple state transition from animation setup

--- src/state_machine/transitions/mod.rs
+++ src/state_machine/transitions/mod.rs
@@ -9,17 +9,30 @@ pub use self::stand::*;
 use crate::prelude::*;
 
 pub fn handle_transitions(world: &mut World) {
+
     let query = world
-        .query_named::<(&mut StateMachine, &ActionData, &mut Animator)>("Handle transitions")
+        .query_named::<(&mut StateMachine, &mut Animator)>("Setup animation")
         .set_cached()
         .build();
-    query.each(|(state, data, animator)| {
-        if let Some(mut next) = state.ctx.next.take() {
+    query.each(|(state, animator)| {
+        if let Some(next) = &state.ctx.next {
+            // Setup animation
+            animator.reset();
+            animator.current = next.name().to_string();
+        }
+    });
+
+    let query = world
+        .query_named::<(&mut StateMachine, &ActionData)>("Handle transitions")
+        .set_cached()
+        .build();
+    query.each(|(state, data)| {
+        if let Some(next) = state.ctx.next.take() {
             // State transition
             state.current.on_exit(&mut state.ctx);
-            state.ctx.elapsed = 1;
-            next.on_enter(&mut state.ctx);
             state.current = next;
+            state.ctx.elapsed = 1;
+            state.current.on_enter(&mut state.ctx);
 
             if let Some(action) = data.get(state.current.name()) {
                 state.ctx.total = action.total;
@@ -35,10 +48,6 @@ pub fn handle_transitions(world: &mut World) {
                     }
                 }
             }
-
-            // Setup animation
-            animator.reset();
-            animator.current = state.current.name().to_string();
         }
     })
 }
